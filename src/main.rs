2207f59c5792105f8d4e9b403ffc2168393b7859
hbobenicio <hbobenicio@gmail.com>
2020-12-18 09:01:47 -0300

    removing buggy tokio and reqwest mess and simplifying to blocking reqwest that just works

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
+mod logging;
+
 use std::path::{Path, PathBuf};
 
 use log::{info, error};
-use tokio::io::AsyncWriteExt;
+use std::io::{Read, Write};
 
 const VSCODE_DOWNLOAD_URL: &str = "https://update.code.visualstudio.com/latest/linux-deb-x64/stable";
 
 fn main() {
-    log_init();
+    logging::init();
 
     if let Err(err) = run() {
         error!("{}", err);
@@ -14,67 +16,51 @@ fn main() {
     }
 }
 
-fn log_init() {
-    if std::env::var_os("RUST_LOG").is_none() {
-        std::env::set_var("RUST_LOG", "info");
-    }
-    env_logger::init();
-}
-
 fn run() -> Result<(), Box<dyn std::error::Error>> {
-    // Tokio 2
-    let mut runtime = tokio::runtime::Builder::new()
-        .threaded_scheduler()
-        .enable_all()
-        .max_threads(4)
-        .build()?;
-
-    runtime.block_on(main_task())?;
-
+    main_task()?;
     Ok(())
 }
 
-async fn main_task() -> Result<(), Box<dyn std::error::Error>> {
+fn main_task() -> Result<(), Box<dyn std::error::Error>> {
     info!("downloading the latest vscode...");
-    let output_file_path: PathBuf = download_vscode().await?;
+    let output_file_path: PathBuf = download_vscode()?;
 
     info!("installing vscode...");
-    install_vscode(&output_file_path).await?;
+    install_vscode(&output_file_path)?;
 
     Ok(())
 }
 
-async fn download_vscode() -> Result<PathBuf, Box<dyn std::error::Error>> {
-    let client: reqwest::Client = reqwest::ClientBuilder::new()
-        .connect_timeout(std::time::Duration::from_secs(10))
-        .connection_verbose(true)
-        .build()?;
+fn download_vscode() -> Result<PathBuf, Box<dyn std::error::Error>> {
 
-    let mut response: reqwest::Response = client.get(VSCODE_DOWNLOAD_URL)
-        .send()
-        .await?
-        .error_for_status()?; // generate an error if server didn't respond OK
+    let mut response: reqwest::blocking::Response = {
+        let client: reqwest::blocking::Client = reqwest::blocking::ClientBuilder::new()
+            .connection_verbose(true)
+            .build()?;
 
-    // Try to get the filename of the last url segment. If it fails, use "vscode.deb" as default
-    let file_name: &str = response.url()
-        .path_segments()
-        .and_then(|segments| segments.last())
-        .and_then(|name| if name.is_empty() { None } else { Some(name) })
-        .unwrap_or("vscode.deb");
+        client.get(VSCODE_DOWNLOAD_URL)
+            .send()?
+    };
 
-    let mut output_file_path = std::env::temp_dir();
-    output_file_path.push(file_name);
+    let mut data: Vec<u8> = Vec::new();
+    response.read_to_end(&mut data)?;
 
-    // Creates a new file and asynchronously writes buffers to it that are comming from the response stream
-    let mut output_file: tokio::fs::File = tokio::fs::File::create(&output_file_path).await?;
-    while let Some(chunk) = response.chunk().await? {
-        output_file.write(&chunk).await?;
-    }
+    let output_file_path = vscode_file_path();
+    let mut output_file: std::fs::File = std::fs::File::create(&output_file_path)?;
+
+    output_file.write_all(&data)?;
 
     Ok(output_file_path)
 }
 
-async fn install_vscode<P>(output_file_path: P) -> Result<(), Box<dyn std::error::Error>>
+fn vscode_file_path() -> PathBuf {
+    let mut path = std::env::temp_dir();
+    path.push("vscode.deb");
+
+    path
+}
+
+fn install_vscode<P>(output_file_path: P) -> Result<(), Box<dyn std::error::Error>>
     where P: AsRef<Path>
 {
     let output_file_path: &str = output_file_path.as_ref()
